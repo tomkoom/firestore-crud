49d93cae013f40d309c8e7f998cc0e05bcb71e14
tomkoom <tomash.sugint@gmail.com>
2022-01-27 22:46:03 +0300

    feat: add email sign-up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+import { useState } from 'react';
 import './App.css';
+
+// components
 import Users from './Components/AddUsers/Users';
+
+// firebase
 import { db } from "./firebase";
-import { auth } from "./firebase";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useAuth } from './Context/AuthContext';
 
 function App() {
-  const { currentUser } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  console.log(currentUser)
+  const { currentUser, register } = useAuth();
+
+  const signUp = () => {
+    register(email, password)
+      .then((userCredential) => console.log(userCredential))
+      .catch((err) => {
+        const errCode = err.code;
+        const errMessage = err.message;
+        console.log(errCode);
+        console.log(errMessage);
+      });
+  }
+
   return (
     <div className="App">
       <button>Sign in with Google</button>
       <p>{JSON.stringify(currentUser)}</p>
+
+      <div>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} name="email" type="email" autoComplete='email' placeholder='Email' />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} name="password" type="password" placeholder='Password' />
+        <button onClick={signUp}>Sign up</button>
+      </div>
+
       <Users db={db} />
     </div>
   );
--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { auth } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth"
 
 const AuthContext = createContext();
 
@@ -10,8 +11,13 @@ export const useAuth = () => {
 export default function AuthContextProvider({ children }) {
 	const [currentUser, setCurrentUser] = useState(null);
 
+	function register(email, password) {
+		return createUserWithEmailAndPassword(auth, email, password);
+	}
+
 	const value = {
-		currentUser
+		currentUser,
+		register,
 	}
 	return (
 		<AuthContext.Provider value={value}>
